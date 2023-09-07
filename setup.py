cc749928c7861a3d7ff5af21fdb22e725d4c4c64
prodata2000 <108673178+prodata2000@users.noreply.github.com>
2023-09-06 19:26:24 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flask-text-generator",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Flask",
+        "Pillow"
+    ],
+    entry_points={
+        'console_scripts': [
+            'flaskapp=your_module_name:app.run'
+        ]
+    }
+)
